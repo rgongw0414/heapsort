885d26684dfe88e20ac908923d3cdb0f85bb305d
rgongw0414 <a1055547@mail.nuk.edu.tw>
2022-04-08 16:01:41 +0800

    first commit

---
+++ hs.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char* argv[]){
+	//int len = atoi(argv[0]);
+	//int* arr = malloc(sizeof(int) * len);
+	int arr[] = {85, 83, 77, 15, 50};
+	int len = sizeof(arr) / sizeof(arr[0]);
+}
+
